19f505d47dbe096a2c8d9f287f65b62dd811f73a
Perfect Delivery <ecmascript.guru@gmail.com>
2019-04-26 02:57:26 +0400

    theme added.

---
+++ assets/scripts/index.js
@@ -0,0 +1,9 @@
+window.$ = window.jQuery = require('jquery')
+
+require('bootstrap')
+require('adminlte')
+require('icheck')
+
+import '../stylesheets/layout.css'
+import '../images/user2-160x160.jpg'
+import '../images/user3-128x128.jpg'
