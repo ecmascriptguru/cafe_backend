6b0d9f56612e63cad57b1a032312dc920358a440
Perfect Delivery <ecmascript.guru@gmail.com>
2019-05-14 00:22:23 +0400

    models created in channels app

--- cafe_backend/mgnt/chat/models.py
+++ cafe_backend/mgnt/chat/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django_fsm import FSMField
+from model_utils.models import TimeStampedModel
+from cafe_backend.core.constants.types import CHAT_ROOM_TYPE
 
-# Create your models here.
+
+CHANNEL_TYPE_CHOICES = (
+    (CHAT_ROOM_TYPE.direct, 'Direct'),
+    (CHAT_ROOM_TYPE.private, 'Private'),
+    (CHAT_ROOM_TYPE.public, 'Public'))
+
+
+class Channel(TimeStampedModel):
+    name = models.CharField(max_length=32)
+    description = models.TextField(max_length=256)
+    channel_type = FSMField(
+        choices=CHANNEL_TYPE_CHOICES, default=CHAT_ROOM_TYPE.private)
+
+    class Meta:
+        ordering = ('created', )
+
+
+class Attendee(TimeStampedModel):
+    channel = models.ForeignKey(
+        Channel, on_delete=models.CASCADE, related_name='attendees')
+    user = models.ForeignKey(
+        'users.User', on_delete=models.CASCADE, related_name='attendees')
+    is_active = models.BooleanField(default=True)
+
+
+class Message(TimeStampedModel):
+    channel = models.ForeignKey(
+        Channel, on_delete=models.CASCADE, related_name='messages')
+    poster = models.ForeignKey(
+        'users.User', on_delete=models.CASCADE, related_name='messages')
+
+    class Meta:
+        ordering = ('-modified', )
