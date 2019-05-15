9aa9f06cbff6cb586abfbcf980b1cce5a4550c01
Perfect Delivery <ecmascript.guru@gmail.com>
2019-05-16 01:32:31 +0400

    asgi added to config

--- cafe_backend/mgnt/chat/routing.py
+++ cafe_backend/mgnt/chat/routing.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import consumers
 
 websocket_urlpatterns = [
-    url(r'^ws/chat/(?P<user_pk>[^/]+)/(?P<channel_pk>[^/]+)/$', consumers.ChatConsumer),
+    url(
+        r'^ws/chat/(?P<user_pk>[^/]+)/(?P<channel_pk>[^/]+)/$',
+        consumers.ChatConsumer),
 ]
