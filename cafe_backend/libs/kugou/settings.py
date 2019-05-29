20901ba53b1c58d9e8ccb358957a7544ba8ca6f1
Perfect Delivery <ecmascript.guru@gmail.com>
2019-05-29 16:35:48 +0400

    draft

---
+++ cafe_backend/libs/kugou/settings.py
@@ -0,0 +1,2 @@
+SONG_SEARCH_API_BASE_URL = 'http://songsearch.kugou.com/song_search_v2?keyword={keyword}&page={page}&pagesize={per_page}&userid=-1'
+SONG_RETRIEVE_API_BASE_URL = 'http://www.kugou.com/yy/index.php?r=play/getdata&hash={file_hash}&album_id={album_id}'
