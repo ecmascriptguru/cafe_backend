f051fd596b28d18a17907a923035c6b61e84d71b
Perfect Delivery <ecmascript.guru@gmail.com>
2019-06-06 19:05:04 +0400

    watermarking feature created in dish image

--- config/settings/base.py
+++ config/settings/base.py
@@ -221,3 +221,6 @@ ASGI_APPLICATION = 'config.routing.application'
 CAFE_BACKEND_TMP_PATH = '%s/tmp' % BASE_DIR
 MUSIC_DOWNLOAD_PATH = "%s/music" % CAFE_BACKEND_TMP_PATH
 DATA_UPLOAD_MAX_MEMORY_SIZE = 100 * 1024 * 1024
+
+# WATERMARK
+WATERMARK_IMAGE = os.path.join(BASE_DIR, 'static', 'watermark.jpg')
