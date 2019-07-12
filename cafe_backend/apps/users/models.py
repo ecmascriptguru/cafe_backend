e82bd85430fd5cf240e8c436e975bd60df49dc47
Perfect Delivery <ecmascript.guru@gmail.com>
2019-07-12 19:13:32 +0400

    table ordering updated.

--- cafe_backend/apps/users/models.py
+++ cafe_backend/apps/users/models.py
@@ -42,7 +42,7 @@ class User(AbstractUser):
 
 class Table(TimeStampedModel):
     class Meta:
-        ordering = ('pk', )
+        ordering = ('user__first_name', )
         verbose_name = _('Table')
         verbose_name_plural = _('Tables')
 
